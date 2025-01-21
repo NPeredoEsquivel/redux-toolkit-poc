0e652a371d2807e03cf98b9a7ad8d433d0176879
Nicolas Peredo <nperedoesquivel@gmail.com>
2025-01-21 12:56:46 -0300

    getting users

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -2,21 +2,34 @@ import { createSlice, PayloadAction, nanoid } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 import { selectCurrentUsername } from '../auth/authSlice';
 
+import { client } from '@/api/client';
+
+import { createAppAsyncThunk } from '@/app/withTypes';
+
 interface User {
   id: string
   name: string
 }
 
-const initialState: User[] = [
-  { id: '0', name: 'Tiago' },
-  { id: '1', name: 'Lucas' },
-  { id: '2', name: 'Rafael' },
-]
+export const fetchUsers = createAppAsyncThunk('users/fetchUsers',
+  async () => {
+    const response = await client.get<User[]>('fakeApi/users')
+    return response.data
+  }
+)
+
+const initialState: User[] = []
+
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return [...action.payload]
+    })
+  }
 })
 
 export const { } = usersSlice.actions
