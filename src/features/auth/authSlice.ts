dd81a804a446518b288bc55b8cdbe6afe3920184
nperedoesquivel <nperedoesquivel@gmail.com>
2025-01-22 15:32:12 -0300

    Adding new thunks when authenticating

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,9 +1,25 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit"
 import { RootState } from "../../app/store"
+
+import { client } from "@/api/client"
+import { createAppAsyncThunk } from "@/app/withTypes"
 
 interface AuthState {
   username: string | null
 }
+
+export const login = createAppAsyncThunk('auth/login',
+  async (username: string) => {
+    await client.post('/fakeApi/authenticate', { username })
+    return username
+  }
+)
+
+export const logout = createAppAsyncThunk('auth/logout',
+  async () => {
+    await client.post('/fakeApi/logout', {})
+  }
+)
 
 const initialState: AuthState = {
   username: null,
@@ -12,21 +28,16 @@ const initialState: AuthState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-    userLoggedIn(state, action: PayloadAction<string>) {
-      return {
-        username: action.payload
-      }
-    },
-    userLoggedOut(state) {
-      return {
-        username: null
-      }
-    }
-  },
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.username = action.payload
+    })
+    builder.addCase(logout.fulfilled, (state) => {
+      state.username = null
+    })
+  }
 })
-
-export const { userLoggedIn, userLoggedOut } = authSlice.actions
 
 export const selectCurrentUsername = (state: RootState) => state.auth.username
 
