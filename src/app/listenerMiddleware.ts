25074cf8f2fe56745a058afc1861ab99a9a41f1e
nperedoesquivel <nperedoesquivel@gmail.com>
2025-01-30 10:56:31 -0300

    Start listening to the add post action

--- src/app/listenerMiddleware.ts
+++ src/app/listenerMiddleware.ts
@@ -1,6 +1,8 @@
 import { createListenerMiddleware, addListener } from "@reduxjs/toolkit"
 import {RootState, AppDispatch } from './store'
 
+import { addPostListener } from '@/features/posts/postsSlice'
+
 export const listenerMiddleware = createListenerMiddleware()
 
 export const startAppListening = listenerMiddleware.startListening.withTypes<RootState, AppDispatch>()
@@ -9,3 +11,5 @@ export type AppStartListening = typeof startAppListening
 
 export const addAppListener = addListener.withTypes<RootState, AppDispatch>()
 export type AppAddListener = typeof addAppListener
+
+addPostListener(startAppListening)
