4fcf96e372d9199417971f9676dda609fa72d64c
nperedoesquivel <nperedoesquivel@gmail.com>
2025-02-05 15:50:51 -0300

    Updating apiSlice to obtain only one post

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -11,8 +11,11 @@ export const apiSlice = createApi({
   endpoints: builder => ({
     getPosts: builder.query<Post[], void>({
       query: () => '/posts'
+    }),
+    getPost: builder.query<Post, string>({
+      query: postId => `posts/${postId}`
     })
   })
 })
 
-export const { useGetPostsQuery } = apiSlice
+export const { useGetPostsQuery, useGetPostQuery } = apiSlice
