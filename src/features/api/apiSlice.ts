e654421cdccbbeda28fe55be63e88431d8b3a4a1
nperedoesquivel <nperedoesquivel@gmail.com>
2025-02-07 10:04:04 -0300

    Refreshing data automatically

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -7,10 +7,12 @@ export const apiSlice = createApi({
   reducerPath: 'api',
   //All of the URLs will start with fakeApi. This also helps the body to be JSON-serialized.
   baseQuery: fetchBaseQuery({ baseUrl: '/fakeApi' }),
+  tagTypes: ['Post'],
   //operations and requests for this server.
   endpoints: builder => ({
     getPosts: builder.query<Post[], void>({
-      query: () => '/posts'
+      query: () => '/posts',
+      providesTags: ['Post'],
     }),
     getPost: builder.query<Post, string>({
       query: postId => `posts/${postId}`
@@ -21,7 +23,8 @@ export const apiSlice = createApi({
         url: '/posts',
         method: 'POST',
         body: initialPost,
-      })
+      }),
+      invalidatesTags: ['Post']
     })
   })
 })
