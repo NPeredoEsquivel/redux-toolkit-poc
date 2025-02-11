7ee10d2dfb4d6b81c7c463b5cfd7ee61e5d419ee
nperedoesquivel <nperedoesquivel@gmail.com>
2025-02-11 09:53:36 -0300

    Migrating store to RTK Query on userSlice

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,12 +1,13 @@
-import { createSlice, createEntityAdapter, PayloadAction, nanoid } from '@reduxjs/toolkit';
+import { createSlice, createEntityAdapter, createSelector } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 import { selectCurrentUsername } from '../auth/authSlice';
 
 import { client } from '@/api/client';
 
 import { createAppAsyncThunk } from '@/app/withTypes';
+import { apiSlice } from '@/features/api/apiSlice';
 
-interface User {
+export interface User {
   id: string
   name: string
 }
@@ -32,11 +33,30 @@ const usersSlice = createSlice({
   }
 })
 
+const emptyUsers: User[] = []
+
+
+//This generates a new selector that will return the query result object for a query with those parameters.
+//To generate a selector for a specific query argument, call 'select(theQueryArg)`.
+//In this case, the users query has no params, so we don't pass anything to select().
+export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+
+export const selectAllUsers = createSelector(
+  selectUsersResult,
+  usersResult => usersResult?.data ?? emptyUsers
+)
+
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state: RootState, userId: string) => userId,
+  (users, userId) => users.find(user => user.id === userId)
+)
+
 export const { } = usersSlice.actions
 export default usersSlice.reducer
 
 
-export const { selectAll: selectAllUsers, selectById: selectUserById  } = usersAdapter.getSelectors((state: RootState) => state.users)
+/* export const { selectAll: selectAllUsers, selectById: selectUserById  } = usersAdapter.getSelectors((state: RootState) => state.users) */
 
 export const selectCurrentUser = (state: RootState) => {
   const currentUsername = selectCurrentUsername(state)
